31017c6918e9d8c8958cc01b15551670e7d06894
Иван Левичев <navisun@optima.local>
2024-10-01 19:40:20 +0300

    Moved 'fortune Cookie' functionality into module

--- main.js
+++ main.js
@@ -2,19 +2,13 @@ import express from "express";
 import { engine } from "express-handlebars";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { getFortune } from "./lib/fortune.js";
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = process.env.port || 3000;
-
-const fortunes = [
-    'Победи свои страхи или они победят тебя',
-    'рекам нуны истоки',
-    'Не бойся неведомого',
-    'Тебя ждет приятный сюрприз',
-    'буд проще везде где только можно'
-]
 
 // Настройка механизма view handlebars
 app.engine(
@@ -31,9 +25,9 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
 app.get("/about", (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render("about", { fortune: randomFortune })
-})
+  
+  res.render("about", { fortune: getFortune() });
+});
 
 // Пользовательская страница 404
 app.use((req, res) => {
@@ -47,13 +41,9 @@ app.use((err, req, res, next) => {
   res.render("500");
 });
 
-
-
 // Запуск сервера
 app.listen(port, () => {
   console.log(
     `Сервер запущен по адресу http://localhost:${port}  Нажмите Cntl + C для завершения работы`
   );
 });
-
-
